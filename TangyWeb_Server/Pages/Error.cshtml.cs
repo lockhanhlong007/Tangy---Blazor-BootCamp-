61bcb9d34ea93b2d3e411cdac9dd7bc3f1b703ef
lockhanhlong007 <62597820+lockhanhlong007@users.noreply.github.com>
2022-02-04 21:15:43 +0700

    Add project files.

---
+++ TangyWeb_Server/Pages/Error.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+
+namespace TangyWeb_Server.Pages
+{
+    /// <summary>
+    /// The error model.
+    /// </summary>
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [IgnoreAntiforgeryToken]
+    public class ErrorModel : PageModel
+    {
+        /// <summary>
+        /// Gets or sets the request id.
+        /// </summary>
+        public string? RequestId { get; set; }
+
+        /// <summary>
+        /// Gets a value indicating whether show request id.
+        /// </summary>
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        private readonly ILogger<ErrorModel> _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ErrorModel"/> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        public ErrorModel(ILogger<ErrorModel> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Ons the get.
+        /// </summary>
+        public void OnGet()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        }
+    }
+}
